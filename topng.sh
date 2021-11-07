7f4c37a9950edcf3fed250aeaa245c0a7c707a90
“yannics” <by.cmsc@gmail.com>
2021-11-07 14:43:06 +0100

    ...

--- topng.sh
+++ topng.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 name=`basename "$1" | cut -d. -f1`
 cat $1 | sed -e '/<!-- stitle -->/,/<!-- etitle -->/d' | sed -e '/<!-- sfooter -->/,/<!-- efooter -->/d' > foo$name.html
-./webkit2png -F -o foo$name -z 2 foo$name.html
+#./webkit2png -F -o foo$name -z 2 foo$name.html
+./wkhtmltoimage-amd64 --width 2048 --zoom 2 foo$name.html foo$name.png
 #/usr/bin/convert
 /opt/local/bin/convert foo$name.png -fuzz 1% -trim +repage -bordercolor white -compose Copy -border 10 $name.png
 rm foo$name.html foo$name.png
